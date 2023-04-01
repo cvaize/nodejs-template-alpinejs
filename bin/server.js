74178a0db1247daf18cbb7fc2abe8d431a7a8041
d.orlov <d.orlov@2-up.ru>
2023-04-01 09:51:31 +0500

    Init

--- bin/server.js
+++ bin/server.js
@@ -1,31 +1,111 @@
+require('dotenv').config()
 const {products} = require('../src/data');
 const http = require('http');
 const ejs = require('ejs');
 const fs = require('fs');
 const path = require('path');
+const {createClient: redisCreateClient} = require('redis');
+const querystring = require('node:querystring');
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
+const redisClient = redisCreateClient({url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`});
+redisClient.on('error', err => console.log('Redis Client Error', err));
+let redisConnected = false;
+redisClient.connect().then(() => redisClient.flushAll()).then(() => redisConnected = true);
+
 const template = fs.readFileSync(path.resolve('src/template/home.ejs'), 'utf-8')
 
-const server = http.createServer((req, res) => {
+const getCachedResponse = async (key, cb, isUseCache = true) => {
+    if (!redisConnected || !isUseCache) {
+        console.log(`Return without cache: ${key}`);
+        return await cb();
+    }
+
+    let response = await redisClient.get(key);
+    if (response) {
+        console.log(`Return from cache: ${key}`);
+        return response;
+    }
+
+    response = await cb();
+    console.log(`Return without cache: ${key}`);
+    await redisClient.set(key, response);
+    return response;
+}
+
+const execTimeout = async (timeout) => {
+    if (timeout) {
+        await new Promise((resolve) => {
+            setTimeout(resolve, Number(timeout));
+        });
+    }
+}
+
+const server = http.createServer(async (req, res) => {
     res.statusCode = 200;
-    if (req.url === '/' && req.method === 'GET') {
+    const url = req.url;
+
+    const splitUrl = url.split('?');
+    const route = splitUrl[0];
+    const params = querystring.parse(splitUrl[1] ?? "");
+
+    console.log(`Request url=${url}; route=${route} params=${JSON.stringify(params)}`)
+    let response = '404: File Not Found';
+
+    if (route === '/' && req.method === 'GET') {
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
 
-        let html = ejs.render(template, {products});
+        response = await getCachedResponse(url, async () => {
+            await execTimeout(params.timeout);
+            return ejs.render(template, {products});
+        }, params?.cache !== '0');
 
-        res.end(html);
-    } else if (req.url === '/api' && req.method === 'GET') {
+    } else if (route === '/api' && req.method === 'GET') {
         res.setHeader('Content-Type', 'application/json; charset=utf-8');
-        res.end(JSON.stringify(products));
+
+        response = await getCachedResponse(url, async () => {
+            await execTimeout(params.timeout);
+            return JSON.stringify(products);
+        }, params?.cache !== '0');
+
     } else {
         res.statusCode = 404;
-        res.end('404: File Not Found');
     }
+    res.end(response);
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+/**
+ * Exit Program
+ */
+
+
+process.stdin.resume();//so the program will not close instantly
+
+async function exitHandler(options, exitCode) {
+    if (options.cleanup) {
+        redisConnected = false;
+        await redisClient.disconnect();
+    }
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null, {cleanup: true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit: true}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {exit: true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit: true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit: true}));
