6b64e5583a41ca038b9ec05f85dd1f93448c69d5
d.orlov <d.orlov@2-up.ru>
2023-04-02 13:07:43 +0500

    Init

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ module.exports = {
                 id,
                 name: id + ". Креветки, партия " + Math.round(Math.random() * limit),
                 description: "Крутые креветки ID: " + id + ". Доставка на номер " + Math.round(Math.random() * limit) + ".",
-                image: `https://via.placeholder.com/${Math.round(Math.random() * 1000)}x${Math.round(Math.random() * 1000)}`
+                image: `https://via.placeholder.com/60x40`
             })
         }
         return products;
